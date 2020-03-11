cf944d9cd7197da0e21a9e8165296b5c67a20a22
daker <523246656@qq.com>
2020-03-11 15:48:11 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
 
---
+++ src/components/DigitalTransform.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "DigitalTransfrom"
+};
+</script>
+
+<style lang="scss" scoped></style>
